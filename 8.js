28fba7efb0abbb57c879acbf8389fb103fc957f8
EmpTyNe <darkstalker2345@gmail.com>
2023-11-12 21:32:58 +0300

    81

---
+++ 8.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+}
+history.pushState({
+    id : "home",
+    source: "web"
+    }, "Задание 8", ""
+)
--- slick/7.js
+++ slick/7.js
@@ -17,4 +17,3 @@ $(window).on('load resize', function(){
     });
 }
 })
-  
