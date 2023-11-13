b6d67c8333772e806fe5132ab1ffd37170917c76
EmpTyNe <darkstalker2345@gmail.com>
2023-11-13 22:00:55 +0300

    81,2

--- 8.js
+++ 8.js
@@ -1,18 +1,34 @@
-$(document).ready(function(){
-    //Скрыть PopUp при загрузке страницы    
-    PopUpHide();
-});
-//Функция отображения PopUp
-function PopUpShow(){
-    $("#popup1").show();
-}
-//Функция скрытия PopUp
-function PopUpHide(){
-    $("#popup1").hide();
-}
-window.history.pushState(
-    {},
-    '',
-    'index.html'
-  )
+function freezeHistory() {
+    window.history.pushState({}, window.document.title, window.location.href);
+  }
   
+  function goBack() {
+    /*
+      Custom history back actions: close panel, close popup, close drop-down menu
+    */
+    var popupOpen = $(".popup.active").length > 0;
+    if(popupOpen) {
+      hidePopup();
+      return false;
+    }
+    window.history.back();
+    return true;
+  }
+  
+  function showPopup() {
+    $('.popup').addClass('active');
+    freezeHistory();
+  }
+  
+  function hidePopup() {
+    $('.popup').removeClass('active');
+  }
+  
+  $(window).on("popstate", function(e) {
+    /* 
+       Browsers tend to handle the popstate event differently on page load. 
+       Chrome (prior to v34) and Safari always emit a popstate event on page load, 
+       but Firefox doesn’t.
+    */
+    goBack();
+  })
