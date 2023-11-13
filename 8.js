2dad8a83bf412303b2c4e3c6c5376b21a94dd3a9
EmpTyNe <darkstalker2345@gmail.com>
2023-11-13 22:37:42 +0300

    81.fin

--- 8.js
+++ 8.js
@@ -1,46 +1,16 @@
-let popupOpen = false;
 $(document).ready(function(){
     //Скрыть PopUp при загрузке страницы    
     PopUpHide();
 });
 //Функция отображения PopUp
 function PopUpShow(){
-    if (popupOpen) {
-        window.history.back();
-    }
-    popupOpen = true;
-    window.history.pushState("forward", null, "");
     $("#popup1").show();
+    window.history.pushState("8",null,' ')
 }
 //Функция скрытия PopUp
 function PopUpHide(){
-    popupOpen = false;
     $("#popup1").hide();
 }
-
-
-// $(".popup-link").click(function() {
-//     showPopup();
-// });
-
-// $(".popup-close").click(function() {
-//     window.history.back();
-// });
-
-// function showPopup() {
-//     if (popupOpen) {
-//         window.history.back();
-//     }
-//     popupOpen = true;
-//     window.history.pushState("forward", null, "");
-//     $(".popup").addClass("active");
-// }
-
-// function hidePopup() {
-//     popupOpen = false;
-//     $(".popup").removeClass("active");
-// }
-
-// $(window).on("popstate", function() {
-//     hidePopup();
-// });
+$(window).on('popstate', function () {
+    PopUpHide();
+});
